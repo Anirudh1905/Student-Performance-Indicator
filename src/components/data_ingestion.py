30870281f77c0a09a4d76a5d381c78c8b75678a1
Anirudh1905 <anirudhsridhar1905@gmail.com>
2023-03-23 13:16:16 +0530

    training code done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,7 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 @dataclass
 class DataIngestionConfig:
     raw_data_path: str = os.path.join('artifacts','raw.csv')
@@ -47,3 +48,6 @@ if __name__== '__main__':
     
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
