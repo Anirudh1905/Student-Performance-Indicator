a4f3753a6cd6598e2e5fbd87d4e868795acb0af9
Anirudh1905 <anirudhsridhar1905@gmail.com>
2023-03-23 18:41:02 +0530

    bux fix in application.py

--- application.py
+++ application.py
@@ -1,13 +1,8 @@
 from flask import Flask,request,render_template
-import numpy as np
-import pandas as pd
-
-from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application=Flask(__name__)
-
-app=application
+application = Flask(__name__)
+app = application
 
 ## Route for a home page
 
@@ -28,15 +23,13 @@ def predict_datapoint():
             test_preparation_course = request.form.get('test_preparation_course'),
             reading_score = float(request.form.get('writing_score')),
             writing_score = float(request.form.get('reading_score'))
-
         )
-        pred_df=data.get_data_as_data_frame()
+        pred_df = data.get_data_as_dataframe()
         print(pred_df)
 
         predict_pipeline = PredictPipeline()
         result = predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=results[0])
-    
+        return render_template('home.html',results=result[0])
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
+    app.run(host="0.0.0.0",debug=True)        
